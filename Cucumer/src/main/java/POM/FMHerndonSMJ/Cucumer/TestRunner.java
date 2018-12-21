66708de0ea553aebb11dbcb0590d3347199b3549
Meseret <meseret.bisrat@gmail.com>
2018-12-21 14:56:11 -0500

    commited

---
+++ Cucumer/src/main/java/POM/FMHerndonSMJ/Cucumer/TestRunner.java
@@ -0,0 +1,11 @@
+package POM.FMHerndonSMJ.Cucumer;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions (features = "Features/VerifyTitle.feature")
+public class TestRunner extends AbstractTestNGCucumberTests{
+	
+	
+
+}
---
+++ Cucumer/src/test/java/POM/FMHerndonSMJ/Cucumer/login_stepdef.java
@@ -0,0 +1,5 @@
+package POM.FMHerndonSMJ.Cucumer;
+
+public class login_stepdef {
+
+}
